a60b324e03fb6544e6b96922d691e24bfbf21297
kevinfontao <kevinfontao@gmail.com>
2020-06-16 23:30:04 +0000

    added jumbotron.js file

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "./navbar";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import Jumbotron from "./jumbotron";
 
 //create your first component
 export function Home() {
 	return (
 		<div className="container">
 			<Navbar />
+			<Jumbotron />
 		</div>
 	);
 }
