7aab980d8f0e005968129e0380bf7f9970abc7b8
Janio Cesar <cesargarcia718@gmail.com>
2020-06-17 23:24:09 +0000

    Added the proper photos to card deck

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -4,9 +4,13 @@ function Card() {
 	return (
 		<div className="card-deck">
 			<div className="card">
-				<img src="..." className="card-img-top" alt="..." />
+				<img
+					src="https://i.redd.it/qm2hkwky0wz21.jpg"
+					className="card-img-top"
+					alt="Hidden Corg"
+				/>
 				<div className="card-body">
-					<h5 className="card-title">Card title</h5>
+					<h5 className="card-title">Brady bunch</h5>
 					<p className="card-text">
 						This is a longer card with supporting text below as a
 						natural lead-in to additional content. This content is a
@@ -20,9 +24,13 @@ function Card() {
 				</div>
 			</div>
 			<div className="card">
-				<img src="..." className="card-img-top" alt="..." />
+				<img
+					src="https://free-classifieds-usa.com/oc-content/uploads/1551/171475.jpg"
+					className="card-img-top"
+					alt="potato"
+				/>
 				<div className="card-body">
-					<h5 className="card-title">Card title</h5>
+					<h5 className="card-title">Potato</h5>
 					<p className="card-text">
 						This card has supporting text below as a natural lead-in
 						to additional content.
@@ -35,9 +43,13 @@ function Card() {
 				</div>
 			</div>
 			<div className="card">
-				<img src="..." className="card-img-top" alt="..." />
+				<img
+					src="https://lh3.googleusercontent.com/proxy/T1QeLY4J0ku5WNKYvEOHp0nRSAD3BC4-x1daHVK6prVXL3Klwo896OvqmlFD36yaVY42f5udgEwAD_uLqJVksJx9ozOylGMAowJEqGWkk-p3Kc5B8RHdBv_TMR9WdaEhYiYc"
+					className="card-img-top"
+					alt="Corgi at the park"
+				/>
 				<div className="card-body">
-					<h5 className="card-title">Card title</h5>
+					<h5 className="card-title">Chicken Nugget</h5>
 					<p className="card-text">
 						This is a wider card with supporting text below as a
 						natural lead-in to additional content. This card has
@@ -52,9 +64,13 @@ function Card() {
 				</div>
 			</div>
 			<div className="card">
-				<img src="..." className="card-img-top" alt="..." />
+				<img
+					src="https://thedailycorgi.com/wp-content/uploads/2017/05/profile_picture_by_pastel_corgi-d9vf8b5.jpg"
+					className="card-img-top"
+					alt="Source corgi"
+				/>
 				<div className="card-body">
-					<h5 className="card-title">Card title</h5>
+					<h5 className="card-title">Corgi</h5>
 					<p className="card-text">
 						This is a longer card with supporting text below as a
 						natural lead-in to additional content. This content is a
