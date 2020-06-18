21f384219e05e75c78cc90c0148a1409685576e5
emilyjmaloney <emilyjean.maloney@gmail.com>
2020-06-18 21:09:25 +0000

    added navbar and footer files

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,13 +1,20 @@
 import React from "react";
-import Navbar from "./navbar";
+import { Navbar } from "./navbar.js";
+import { Footer } from "./footer.js";
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
+
 export function Home() {
 	return (
-		<div className="container">
-			<Navbar />
+		<div className="webpage-container">
+			<div className="container-fluid">
+				<Navbar />
+			</div>
+
+			<div className="container-fluid">
+				<Footer />
+			</div>
 		</div>
 	);
 }
